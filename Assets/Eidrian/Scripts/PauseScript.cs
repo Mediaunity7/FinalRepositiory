8b9287d311afc443a37e20319ff3838053c00ed9
Mediaunity7 <mediaunity7@gmail.com>
2023-12-06 15:12:08 +0000

    Pause system-Eidrian

---
+++ Assets/Eidrian/Scripts/PauseScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScrypt : MonoBehaviour
+{
+    private GameManager gameManager;
+    [Header("Drag the pause canvas here!")]
+    public GameObject pauseScreen;
+
+    bool isPause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager =GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPause = !isPause;
+            gameManager.Pause(isPause);
+            pauseScreen.SetActive(isPause);
+
+        }   
+    }
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene(0);
+        //Destroy(gameManager);
+    }
+
+}
